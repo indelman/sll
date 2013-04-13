4cebeb906d1c81ec6b4b316cdd3adfe4b85b9e8f
Siddharth Choudhary <siddharth.choudhary@gatech.edu>
2013-04-12 23:18:48 -0400

    Updated bow code

--- localize_cpp/localize.cpp
+++ localize_cpp/localize.cpp
@@ -12,8 +12,25 @@
 #include "fmatrix.h"
 #include "vector.h"
 #include <stdlib.h>
+#include "compute_scores.h"
+#include "gtsam/geometry/SimpleCamera.h"
+#include "ransac/estimateCamera.h"
+
+#include "matrix.h"
+//#include "gtsam
 
 using namespace std;
+
+int estimate_error =0;
+vector < vector <double> > bow_index;
+vector < vector <int> > db_index;
+bundle* poses;
+list< pair <gtsam::Point3, gtsam::Point2> > corr;
+vector < pair <int, int> > dimensions;
+
+list < gtsam::Pose3 > actual_poses;
+
+//void computeError(
 
 /* Estimate an F-matrix from a given set of point matches */
 std::vector<int> EstimateFMatrix(const std::vector<Keypoint> &k1,
@@ -138,32 +155,155 @@ std::vector<int> EstimateFMatrix(const std::vector<Keypoint> &k1,
 
 }
 
+int findNextImage(vector<double> bow_indices)
+{
+    double min = DBL_MAX;
+    int index = -1;
+//    cout << endl << "----------------------------" << endl;
+    for(int i=0; i<bow_indices.size(); i++)
+    {
+    //    cout << bow_indices[i] << " ";
+//        cout << bow_indices[i] << " ";
+         if( bow_indices[i] !=-1 && bow_indices[i] < min)
+         {
+         
+             min = bow_indices[i];
+             index = i;
+         }
+    }
+
+//    cout << endl << "----------------------------" << endl;
+
+    cout << "Min: " << min << " Index: " << index << endl;
+    if(index == -1)return -1;
+    return index;
+}
+
+
+void computeError(int id, int index, vector<Keypoint> Keypoint_q, vector<Keypoint> Keypoint_r, vector<KeypointMatch> matches, vector<int> inliers)
+{
+
+    corr.clear(); // comment it if you want union
+
+    for(int i=0; i<inliers.size(); i++)
+    {
+        int idx1=matches[i].m_idx1;
+        int idx2=matches[i].m_idx2;
+
+        
+       float x0= Keypoint_q[idx1].m_x;
+       float y0 = Keypoint_q[idx1].m_y;
+
+       float x1 = Keypoint_r[idx2].m_x;
+       float y1 = Keypoint_r[idx2].m_y;
+
+       float W = dimensions[id].first;
+       float H = dimensions[id].second;
+
+        int point_index = poses->getPoint(index, idx2);
+
+        if(point_index != -1)
+        {
+                x0 = x0 - W/2;
+                y0 = H/2 - y0;
+                float X = poses->vertexset[point_index].m_pos[0];
+                float Y = poses->vertexset[point_index].m_pos[1];
+                float Z = poses->vertexset[point_index].m_pos[2];
+
+                corr.push_back(make_pair (gtsam::Point3(X,Y,Z), gtsam::Point2(x0,y0)));
+
+//            corr.push_back
+        }
+    }
+
+
+    // estimate Pose
+
+    gtsam::SimpleCamera cam = estimateCamera(corr);
+
+    gtsam::Cal3_S2 K = cam.calibration();
+    gtsam::Pose3 Pose = cam.pose();
+    gtsam::Point3 point = Pose.translation();
+    gtsam::Rot3 rot = Pose.rotation();
+
+
+
+    
+}
+
+
+
 /* Match the keys */
-vector < pair<int, int> > matchKeys( vector<int> indices, const vector<string>& key_files, int ret_cnt)
+vector < pair<int, pair<int,int> > > matchKeys( vector<int> indices, vector<double> bow_indices, vector<int> db_indices, const vector<string>& key_files, int ret_cnt, int id )
 {
 
     float ratio = 0.7;
 
     double F[9];
+    
     int query=indices[0]; // query image index
 
-    unsigned char **keys = new unsigned char *[indices.size()];
-    int *num_keys = new int[indices.size()];
-    keypt_t** info = new keypt_t*[indices.size()];
+    
+        unsigned char **query_keys = new unsigned char*[1];
+        int query_num_keys;
+        keypt_t** query_info = new keypt_t*[1];
 
-    vector < vector < Keypoint > > KeypointVec;
-    for(int i=0; i < ret_cnt; i++)
-    {
-        num_keys[i] = ReadKeyFile(key_files[indices[i]].c_str(), keys+i, info+i); // Read Key files
+        query_num_keys = ReadKeyFile(key_files[indices[0]].c_str(), query_keys, query_info); // Read Key files
 
+     //   cout << query_num_keys << endl;
+
+       // cout << key_files[indices[0]].c_str() << endl;
+
+       double* dist_arr = new double[db_indices.size()];
         // create keypoint vector
-        vector < Keypoint > keypoint_temp;
-        keypt_t* info_t = info[i];
-        unsigned char* key_t = keys[i];
+        vector < Keypoint > Keypoint_q;
 
-        for(int j=0;j< num_keys[i]; j++)
+        keypt_t* query_info_t = query_info[0];
+                        unsigned char* query_key_t = query_keys[0];
+
+        for(int j=0;j< query_num_keys; j++)
         {
-            keypoint_temp.push_back( Keypoint(info_t[j].x, 
+
+            Keypoint_q.push_back( Keypoint(query_info_t[j].x,
+                        query_info_t[j].y,
+                        query_info_t[j].scale,
+                        query_info_t[j].orient,
+                        (short int*) query_key_t + 128*j
+                        ));
+
+        }
+
+
+    // Create the search tree of the query image
+    //ANNkd_tree *tree = CreateSearchTree(num_keys[0],keys[0]);	
+
+    // Create Result vector
+    vector < pair < int, pair<int,int> > > result;
+
+    // Match query image with the rest of the images
+    for(int i=1;i<ret_cnt; i++)
+    {
+
+        cout << "COUNT " << i << endl;
+        int index = findNextImage(bow_indices);
+        if(index == -1)break;
+        unsigned char **keys = new unsigned char*[1];
+        int num_keys;
+        keypt_t** info = new keypt_t*[1]; 
+
+        num_keys = ReadKeyFile(key_files[indices[index+1]].c_str(), keys, info); // Read Key files
+
+ //       cout << key_files[indices[index+1]].c_str() << endl;
+   //     cout << num_keys << endl;
+        keypt_t* info_t = info[0];
+        unsigned char* key_t = keys[0];
+        // create keypoint vector
+        vector < Keypoint > Keypoint_r;
+
+        for(int j=0;j< num_keys; j++)
+        {
+//            cout << info_t[j].x << " " << info_t[j].y << endl;
+            Keypoint_r.push_back( Keypoint(info_t[j].x,
                         info_t[j].y,
                         info_t[j].scale,
                         info_t[j].orient,
@@ -171,24 +311,18 @@ vector < pair<int, int> > matchKeys( vector<int> indices, const vector<string>& 
                         ));
 
         }
-        KeypointVec.push_back(keypoint_temp);
+    
 
-    }
-    // Create the search tree of the query image
-    //ANNkd_tree *tree = CreateSearchTree(num_keys[0],keys[0]);	
 
-    // Create Result vector
-    vector < pair < int, int > > result;
-
-    // Match query image with the rest of the images
-    for(int i=1;i<ret_cnt; i++)
-    {
+        
         // create the tree
-        ANNkd_tree *nn_tree = CreateSearchTree(num_keys[i],keys[i]);
+        ANNkd_tree *nn_tree = CreateSearchTree(num_keys,keys[0]);
 
         std::vector<KeypointMatch> matches =
-           MatchKeys(num_keys[0], keys[0], nn_tree, ratio);
+           MatchKeys(query_num_keys, query_keys[0], nn_tree, ratio);
 
+        
+        bow_indices[index]=-1;
 
         if( (int)matches.size() >= 16)
         {
@@ -199,32 +333,122 @@ vector < pair<int, int> > matchKeys( vector<int> indices, const vector<string>& 
             // normalize x,y locations according to image dimensions
 
             // TODO: normalize it later, right now run according un normalized constraints
-            const vector<int>& inliers = EstimateFMatrix(KeypointVec[0], KeypointVec[i], matches, 2048, 9.0f, F, false);		
+
+            const vector<int>& inliers = EstimateFMatrix(Keypoint_q, Keypoint_r, matches, 2048, 9.0f, F, false);		
+
             // push into result vector
-            result.push_back(make_pair(matches.size(), inliers.size()));
+            result.push_back(make_pair(db_indices[index], make_pair(matches.size(), inliers.size())));
+
+
+            if(inliers.size() >=12 && estimate_error ==1)
+            {
+                // estimate error
+                computeError(id, db_indices[index], Keypoint_q, Keypoint_r, matches, inliers);
+
+            }
+
+            
+            //            if(inliers.size() <12 )
+
+            //          {
+
+            double max_dist = 0;
+            double max_bow = 0;
+            for(int j=0; j<db_indices.size(); j++)
+            {
+                   double dist = poses->getSimilarityDist(db_indices[index], db_indices[j]);
+                   dist_arr[j] = log(1+dist); 
+                   if(dist > max_dist)max_dist = dist;
+
+                   if(bow_indices[j]>max_bow)max_bow = bow_indices[j];
+            }
+
+//            cout << "MAX BOW " << max_bow << " MAX DIST " << max_dist << endl;
+
+               
+            double sum_dist = 0;
+            for(int j=0; j< db_indices.size(); j++)
+            {
+
+                if(bow_indices[j] !=-1)
+                {
+                    double dist = dist_arr[j]/max_dist;
+                    bow_indices[j] = bow_indices[j]/max_bow;
+
+                    if(inliers.size() < 12)
+                        bow_indices[j] = bow_indices[j]*1.0f/dist;
+                    else
+                        bow_indices[j] = bow_indices[j]*dist;
+                }
+            }
+
+            bow_indices[index] = -1;
+
+            // rerank the rest of the images
+            //        }
+            cout << "Found " << matches.size() << " " << inliers.size() << " inliers between 0 and " << index+1 << endl;
 
         }
         else
         {
-            result.push_back(make_pair(matches.size(), -1));
-         }
+            // bow score: bow_indices[i-1];
+            // db_indices: db_indices[i-1];
+
+            // if not matched rerank rest of the images
+            result.push_back(make_pair(db_indices[index], make_pair(matches.size(),-1)));
+
+            double max_dist = 0;
+            double max_bow = 0;
+            for(int j=0; j<db_indices.size(); j++)
+            {
+                double dist = poses->getSimilarityDist(db_indices[index], db_indices[j]);
+                dist_arr[j] = log(1+dist);
+                if(dist > max_dist)max_dist = dist;
+
+                if(bow_indices[j]>max_bow)max_bow = bow_indices[j];
+            }
 
 
-         annDeallocPts(nn_tree->pts);
+
+//            cout << "MAX BOW " << max_bow << " MAX DIST " << max_dist << endl;
+            // cout << endl << "----------------------------" << endl;
+            for(int j=0; j< db_indices.size(); j++)
+            {
+                if(bow_indices[j] != -1)
+                {
+                double dist = dist_arr[j]/max_dist;
+
+  //              cout << bow_indices[j] << " (" << dist << " " << dist_arr[j] << " " << max_dist << ") ";
+                bow_indices[j] = bow_indices[j]/max_bow;
+
+                    bow_indices[j] = bow_indices[j]*1.0f/dist;
+                }
+
+            }
+            //cout << endl << "----------------------------" << endl;
+
+
+            bow_indices[index] = -1;
+
+
+        }
+
+
+        annDeallocPts(nn_tree->pts);
         delete nn_tree;
+        delete [] keys[0];
+        delete [] info[0];
+        delete [] keys;
+        delete [] info;
     }
 
 
-    for(int i=0; i < ret_cnt; i++)
-    {
-        delete [] keys[i];
-        delete [] info[i];
-    }
-    delete [] keys;
-    delete [] info;
+    delete [] query_keys[0];
+    delete [] query_info[0];
+        delete [] query_keys;
+        delete [] query_info;
+
     //delete tree;
-    delete [] num_keys;
-    KeypointVec.clear();
     return result;
 }
 
@@ -256,30 +480,117 @@ const vector<string> readList(char* list_in, char* dir)
 
 }
 
+
+void readGT(char* list_in)
+{
+	ifstream infile;
+	infile.open (list_in);
+    double R[9], t[3], R_temp[9], t_final[3];
+    double number;
+	while(!infile.eof()) // To get you all the lines.
+	{
+
+        
+        for(int i=0; i<9; i++)
+            infile >> R[i];
+
+         for(int i=0;i<3;i++)
+         {
+             infile >> t[i];
+             t[i] = -1*t[i];
+         }
+
+        
+        matrix_invert(3, R, R_temp);
+        matrix_product(3,3,3,1,R_temp, t, t_final); 
+    
+        gtsam::Rot3 rot = gtsam::Rot3(R[0], R[1], R[2], R[3], R[4], R[5], R[6], R[7], R[8]);
+        gtsam::Point3 point = gtsam::Point3(t_final[0], t_final[1], t_final[2]);    
+        actual_poses.push_back( gtsam::Pose3(rot, point));
+
+       
+    }
+
+    //return dimensions;
+}
+
+
+/* Read the dimension */
+
+void readDim(char* list_in)
+{
+
+	ifstream infile;
+	infile.open (list_in);
+    int number1, number2;
+	while(!infile.eof()) // To get you all the lines.
+	{
+
+           infile >> number1;
+           infile >> number2;
+
+           dimensions.push_back(make_pair(number1, number2));
+
+
+    }
+
+    //return dimensions;
+}
+
 /* Read the index list */
 
 const vector< vector<int> > readIndex(char* list_in)
 {
 
 	vector < vector <int> > list_index;
-
+    printf("%s\n",list_in);
 	string str;
 	ifstream infile;
 	infile.open (list_in);
 	while(!infile.eof()) // To get you all the lines.
 	{
 		vector < int > temp;
+		vector < double > bow_temp;
+		vector < int > db_temp;
 		getline(infile,str); // Saves the line in STRING.
 		std::stringstream ss;
 		ss << str;
+
+        int flag = 0;
 		while(!ss.eof())
 		{
 			int number;
+            double numb_t;
 			ss >> number;
+
 			temp.push_back(number);
+            if(flag == 1)
+            {
+
+               ss >> number;
+               db_temp.push_back(number);      
+
+               ss >> numb_t;
+               bow_temp.push_back(numb_t);      
+             
+            }
+
+           flag = 1;
+                
 		}
 		temp.erase(temp.end()-1);
+
+        if(bow_temp.size()>=1)
+		bow_temp.erase(bow_temp.end()-1);
+
+        if(db_temp.size()>=1)
+            db_temp.erase(db_temp.end()-1);
+  //      list_index.erase(list_index.end() - 1);
+    //    db_index.erase(db_index.end() - 1);
+        
 		list_index.push_back(temp);
+		bow_index.push_back(bow_temp);
+		db_index.push_back(db_temp);
 	}
 	infile.close();
 
@@ -297,13 +608,25 @@ int main(int argc, char* argv[])
 	char* dir = argv[3]; //directory of the dataset
 	const vector<string>& key_files = readList(argv[1],dir);
 
+    if(strcmp(dir, "../../data/Dubrovnik6K//")==0 || strcmp(dir,"../../data/Rome16K//")==0)
+    {
+        estimate_error=1;
+        readDim(argv[7]);
+
+        char bundle_file[256];
+        sprintf(bundle_file,"%s/bundle/bundle.query.out",dir);
+        readGT(bundle_file);
+    }
 	// Read the indices
 	const vector< vector<int> >& index_file = readIndex(argv[2]);
+    cout << "size of list_index " << db_index.size() << endl;
+    // Read the bundle file
+    poses = new bundle(argv[4]);
 
 	// Opens the file to write results
-	ofstream f_out(argv[4]);
+	ofstream f_out(argv[5]);
 	
-    int ret_cnt= strtol(argv[5],NULL,10); // retrieve argv[5] images
+    int ret_cnt= strtol(argv[6],NULL,10); // retrieve argv[5] images
 
 	// Match Keys
 	for(int i=0;i<index_file.size()-1; i++)
@@ -311,11 +634,12 @@ int main(int argc, char* argv[])
 		for(int j=0;j<ret_cnt;j++)
 			cout << index_file[i][j] << " ";
 		cout << endl;
-		const vector < pair <int, int> >& result = matchKeys(index_file[i],key_files,ret_cnt);
+		const vector < pair <int, pair<int,int> > >& result = matchKeys(index_file[i], bow_index[i], db_index[i],  key_files,ret_cnt, i);
 
 		for(int j=0;j<result.size(); j++)
-			f_out << result[j].first << " " << result[j].second << " ";
+			f_out << result[j].first << " " << result[j].second.first  << " " << result[j].second.second << " ";
 		f_out << endl;
+//        break;
 	}
 	f_out.close();
 
